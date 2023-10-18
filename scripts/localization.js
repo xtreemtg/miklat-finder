c8bbda2eab9f073b456654ea78c9d77d20dbe422
Yehuda Broderick <46563066+LeggoMahEggo@users.noreply.github.com>
2023-10-18 12:12:20 +0300

    Add localization code

---
+++ scripts/localization.js
@@ -0,0 +1,30 @@
+// Still in development
+function localizePage() {
+    const localeElements = document.querySelectorAll("[locale-value]");
+
+    for (let i = 0; i < localeElements.length; i++) {
+        const localeElement = localeElements[i];
+
+        try {
+            if (localeElement.tagName == "INPUT")
+                localeElement.setAttribute("placeholder", EN_JSON[localeElement.getAttribute("locale-value")]);
+            else {
+                if (localeElement.tagName == "A")
+                    localeElement.setAttribute("href", EN_JSON[localeElement.getAttribute("locale-value")]);
+
+                // Handle initial element being a bold tag
+                var nodeToChange = localeElement.childNodes[0];
+
+                if (nodeToChange.tagName == "B")
+                    nodeToChange = localeElement.childNodes[1];
+
+                nodeToChange.nodeValue = EN_JSON[localeElement.getAttribute("locale-value")];
+            }
+        } catch (error) {
+            // remove for live release
+            alert(`Error setting locale value for ${localeElement.getAttribute("locale-value")}`);
+        }
+    }
+}
+
+localizePage();
