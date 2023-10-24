56eb23a94957965a68d1c34856d581c8bd04e928
Yehuda Broderick <46563066+LeggoMahEggo@users.noreply.github.com>
2023-10-24 15:12:06 +0300

    Add simple function to set svg icon data

--- scripts/map-api.js
+++ scripts/map-api.js
@@ -44,6 +44,18 @@ function panToMapLocation(map, lat, lng) {
 }
 
 // Marker API functions
+function svgData(path, color) {
+    return {
+        path: path,
+        fillColor: color,
+        fillOpacity: 1,
+        strokeWeight: 1,
+        rotation: 0,
+        scale: 2,
+        anchor: new google.maps.Point(0, 20),
+    };
+}
+
 function svgMarkerData(svgPath="", color="blue", opacity=0.6, weight=0) {
     const svgMarker = {
         defaultPath: "M 0 0 q 2.906 0 4.945 2.039 t 2.039 4.945 q 0 1.453 -0.727 3.328 t -1.758 3.516 t -2.039 3.07 t -1.711 2.273 l -0.75 0.797 q -0.281 -0.328 -0.75 -0.867 t -1.688 -2.156 t -2.133 -3.141 t -1.664 -3.445 t -0.75 -3.375 q 0 -2.906 2.039 -4.945 t 4.945 -2.039 z", // Default marker with nothing inside
